9b28b4c8d991439e045f85a896e6163318811865
Ajmal-HH <ajmalajjuartz@gmail.com>
2024-07-03 11:28:38 +0530

    added atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose'
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect('mongodb://0.0.0.0:27017/MOTOMOTION')
+        // await mongoose.connect('mongodb://0.0.0.0:27017/MOTOMOTION')
+        await mongoose.connect('mongodb+srv://ajmalajjuartz:8CXAspYhBhL2c6Ch@cluster0.qpree39.mongodb.net/MOTO-MOTION')
         .then(()=>{
             console.log('mongodb connected');
         })
