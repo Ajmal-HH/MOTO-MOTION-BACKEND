fc984689af8a78553a073120412f9c05fb1d455c
Ajmal-HH <ajmalajjuartz@gmail.com>
2024-07-10 15:35:42 +0530

    code  updated

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ import cookieSession from 'cookie-session';
 import { app, server } from './socket/socket.js';  // Importing from socket.js
 import sessionSecret from './config/config.js';
 
+// Import middleware
+import { is_blocked, userAuth } from './middleware/userAuth.js';
+import protectedRoute from './middleware/protectedRoute.js';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
@@ -35,30 +39,12 @@ app.use(session({
   resave: false
 }));
 
-//Middleware
+// Middleware
 app.use(cors({
   origin: ['https://moto-motion-frontend.vercel.app','https://moto-motion-frontend-ozw2pv646-mohamed-ajmals-projects.vercel.app'],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true
 }));
-
-// const allowedOrigins = [
-//   'https://moto-motion-frontend.vercel.app',
-//   'https://moto-motion-frontend-ozw2pv646-mohamed-ajmals-projects.vercel.app'
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-//   credentials: true
-// }));;
 
 // User Routes
 app.use('/api/', userRouter);
@@ -67,7 +53,7 @@ app.use('/api/bikeowner', bikeOwnerRouter);
 // Admin Routes
 app.use('/api/admin', AdminRouter);
 // Message Routes
-app.use('/api/messages', MessageRouter); 
+app.use('/api/messages', MessageRouter);
 
 app.use((err, req, res, next) => {
   console.log("Error in middleware", err);
