867152a484ee8278fe4568a74de5b902ab85b00c
Ajmal-HH <ajmalajjuartz@gmail.com>
2024-07-18 00:16:47 +0530

    updated

--- middleware/protectedRoute.js
+++ middleware/protectedRoute.js
@@ -1,32 +1,35 @@
-import jwt from 'jsonwebtoken'
-import User from '../model/userModel.js'
+import jwt from 'jsonwebtoken';
+import User from '../model/userModel.js';
 
-const protectedRoute = async (req,res,next) =>{
+const protectedRoute = async (req, res, next) => {
     try {
         const token = req.header('Authorization');
-        console.log(token,"token in protected route ?????");
+        console.log(token, "token in protected route ?????");
 
-        if(!token){
-            res.status(401).json({error : 'Unauthorized-no token'})
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized - no token provided' });
         }
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
 
-        if(!decoded){
-            res.status(401).json({error : 'Unauthorized-no token'})
-        }
-        
-        console.log(decoded,"decoded in protected route>><<<");
-        const user = await User.findById(decoded.userId).select("-password")
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        if(!user){
-            return res.status(404).json({error :  "user nor found"})
+        if (!decoded) {
+            return res.status(401).json({ error: 'Unauthorized - token is not valid' });
         }
-        req.user = user
-        next()
+
+        console.log(decoded, "decoded in protected route>><<<");
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        req.user = user;
+        next();
     } catch (error) {
-        console.log('error from protectedRoute middleware',error.message)
-        res.status(500).json({error : 'Internal server error'})
+        console.log('Error from protectedRoute middleware', error.message);
+        return res.status(500).json({ error: 'Internal server error' });
     }
-}
+};
 
 export default protectedRoute;
